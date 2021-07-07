b63edbb65395ce0ef9be004c143037a5b3092470
Lbreidenbach <80410543+Lbreidenbach@users.noreply.github.com>
2021-07-07 15:57:22 -0500

    Update data_to_dag.R

--- data_to_dag.R
+++ data_to_dag.R
@@ -3,7 +3,7 @@
 library(bnlearn)
 
 
-wbc_df = read.table("~/server/sealockj/projects/mdd_wbc_long/problem_list_meds/time_censor/time_censor_meds_and_labs/pgs_moderation/datasets/mdd_pgs_moderation_between_wbc_and_TCA_365_days_no_inpt_dataset_2021-06-23.txt", 
+df = read.table("~/server/sealockj/projects/mdd_wbc_long/problem_list_meds/time_censor/time_censor_meds_and_labs/pgs_moderation/datasets/mdd_pgs_moderation_between_wbc_and_TCA_365_days_no_inpt_dataset_2021-06-23.txt", 
                 header = TRUE)
 
 
@@ -18,13 +18,70 @@ reclassify = function(df){
   return(df)
 }
 
-wbc_df = reclassify(wbc_df)
-wbc_df_sub = wbc_df[c(-1, -2, -6:-18, -22, -26, -27, -30, -31)] #cutting out all numeric data for now. figure out how to incorporate numerics later
+df = reclassify(df)
+num_true = sapply(df, is.numeric)
+num_ind = as.numeric(unname(which(num_true, useNames = FALSE)))
+#df[num_ind]
 
-dag = mmpc(wbc_df_sub, undirected = FALSE)
-plot(dag)
+#applies all discretize methods to numeric data
+list_df = lapply(X = c("interval", "quantile", "hartemink"), 
+                 FUN = function(method) discretize(
+                   data = df[num_ind],
+                   method = method,
+                   breaks = 4,
+                   ordered = TRUE
+                 ))
 
-#fig
+names(list_df) = c("interval", "quantile", "hartemink")
 
-med_1_df = df[df$med_tvc == 1, ]
-med_0_df = df[df$med_tvc == 0, ]
+#wbc_df_sub = wbc_df[c(-1, -2, -6:-18, -22, -26, -27, -30, -31)] #cutting out all numeric data for now. figure out how to incorporate numerics later
+#v_algorithms = c("pc.stable", "gs", "iamb", "fast.iamb", "inter.iamb", "iamb.fdr", "mmpc", 
+#                 "si.hilton.pc", "hpc", "hc", "tabu", "rsmax2", "mmhc", "h2pc", "aracne", "chow.liu")
+#slower tests, iamb (gs breaks R), hpc and h2pc takes a few seconds, hc doesn't work
+v_algorithms = c("fast.iamb", "mmpc", "si.hiton.pc", "pc.stable", "iamb.fdr", "hpc", "hc", "tabu", "rsmax2", "mmhc", "h2pc", "aracne", "chow.liu")
+
+list_bnlearn = list()
+
+for(j in v_algorithms) for(k in names(list_df))try({
+  list_bnlearn [[j]][[k]] = do.call(
+    what = j,
+    args = list(x = list_df[[k]])
+  )
+})
+
+# df_arcs = arcs(list_bnlearn[[j]][[k]])
+# for(l in 1:row(df_arcs)){
+#   list_bnlearn[[j]][[k]] = set.arc(
+#     
+#   )
+# }
+######Scoring
+M_score <- matrix(
+  data = NA,
+  nrow = length(v_algorithms),
+  ncol = length(list_df),
+)
+rownames(M_score) <- v_algorithms
+colnames(M_score) <- names(list_df)
+
+for(j in v_algorithms) for(k in names(list_df)) try({
+  M_score[j,k] <- score(
+    x = list_bnlearn[[j]][[k]],
+    data = list_M[[k]],
+    type = "bic"
+  )
+})
+for(j in rownames(M_score)) M_score <- M_score[,order(M_score[j,])]
+for(j in colnames(M_score)) M_score <- M_score[order(M_score[,j]),]
+M_score
+#figure out scoring tommorrow
+
+# dag = mmpc(wbc_df_sub, undirected = FALSE, debug = TRUE)
+# plot(dag)
+# 
+# #fig
+# 
+# med_1_df = df[df$med_tvc == 1, ]
+# med_0_df = df[df$med_tvc == 0, ]
+
+
